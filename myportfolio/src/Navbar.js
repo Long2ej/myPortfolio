1da2c038e244ec28265894c911db9b9e48f5682a
Long2ej <long2ej@dukes.jmu.edu>
2023-09-28 14:01:59 -0400

    Routing

--- myportfolio/src/Navbar.js
+++ myportfolio/src/Navbar.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
+function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
 
+    const handleMenuToggle = () => {
+        setMenuOpen(!menuOpen);
+    };
 
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
 
-export default function Navbar() {
     return (
-        <nav className='nav'>
-            <a href='/home' className='site-title'>Title</a>
-            <ul>
-                <li ><a href='/home'>About Me</a></li>
-                <li><a href='/projects'>Projects</a></li>
-            </ul>
-        </nav>
-    )
+        <header>
+            <div className="header">
+                <button onClick={handleMenuToggle} className="show-menu-btn">
+                    <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
+                </button>
+
+                <ul className={`menu ${menuOpen ? 'open' : ''}`} style={{ right: menuOpen ? '0' : '-100%' }}>
+                    <Link to="/home" onClick={closeMenu}>About Me</Link>
+                    <Link to="/projects" onClick={closeMenu}>Projects</Link>
+                </ul>
+            </div>
+        </header>
+    );
 }
 
-
+export default Navbar;
--- myportfolio/src/pages/Home.js
+++ myportfolio/src/pages/Home.js
@@ -1,6 +1,13 @@
 export default function Home() {
     return (
-        <h1>Home</h1>
+
+        <>
+            <div className="home">
+                <h1>Home</h1>
+                <p>  </p>
+
+            </div>
+        </>
     )
 }
 
